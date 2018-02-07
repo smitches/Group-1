45d2d90048b9dacde826afdbc57e24a4a85dfe53
smitches <31672246+smitches@users.noreply.github.com>
2018-02-07 01:34:09 -0600

    python makes allwords file

---
+++ group_1_assignment3/extract_words.py
@@ -0,0 +1,19 @@
+def main():
+    in_file = open("tale_of_two_cities.txt","r")
+    allwords=open("allwords.txt","w")
+    unique=open("uniquewords.txt","w")
+    freq=open("wordfrequency.txt","w")
+    for line in in_file:
+        wordlist=line.split()
+        for word in wordlist:
+            word=word.lower()
+            writeword=''
+            for character in word:
+                if character.isalpha:
+                    writeword+=character
+            allwords.write(word+'\n')
+            
+    allwords.close()
+    unique.close()
+    freq.close()
+main()
